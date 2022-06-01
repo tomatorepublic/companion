bf25be7d698376e7057a073a8e902dab753ac4da
Danya <96995375+tomatorepublic@users.noreply.github.com>
2022-06-01 20:23:32 +0200

    base for the readline

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+rl.question(
+  '1 - Learn new words 2 - Check the word meaning\n',
+  function (input) {
+    switch (input) {
+      case '1': {
+        console.log('Learning new words');
+        break;
+      }
+      case '2': {
+        console.log('Checking the meaning');
+        break;
+      }
+      default: {
+        rl.close();
+      }
+    }
+  }
+);
