9840b260682b95fd7e7f4f35fdcf83f36b477c7c
Danya <96995375+tomatorepublic@users.noreply.github.com>
2022-06-03 00:03:46 +0200

    adding the readline question promise

--- promises.js
+++ promises.js
@@ -6,3 +6,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const question = (str) => new Promise(resolve => rl.question(str, resolve));
+
+module.exports = { question };
