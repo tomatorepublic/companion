833a55dc6e42b8c294a953e54e697bcb39c7db85
Danya <96995375+tomatorepublic@users.noreply.github.com>
2022-06-03 10:02:44 +0200

    switch case method for the starting question

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const { question } = require('./promises.js');
+const learning = require('./learning.js');
+const meaning = require('./meaning.js');
+
+const main = async () => {
+  let selection = parseInt(
+	  await question('1 - Learn new words 2 - Check the word meaning\n')
+	);
+	switch (selection) {
+	  case 1:
+	    learning.main();
+	    break;
+	  case 2:
+	    meaning.main();
+	    break;
+	  default:
+	    process.exit();
+	    break;
+	}
+}
+
+main();
