a29fbf35ed147d2ac667945de8c4c15c2e0bcf74
Danya <96995375+tomatorepublic@users.noreply.github.com>
2022-06-03 14:48:31 +0200

    minor change in the starting question structure

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const learning = require('./learning.js');
 const meaning = require('./meaning.js');
 
 const features = [learning.main, meaning.main];
+const questions = `What can I help you with?
+  1 - Learn new words
+  2 - Check the word meaning
+  3 - Exit an application`;
 
 const main = async () => {
   let selection = parseInt(
-	  (await question('1 - Learn new words 2 - Check the word meaning\n')) - 1
+	  (await question(`${questions}\nI'm interested in - `)) - 1
 	);
 	let answer = features[selection];
 	if (answer) answer();
