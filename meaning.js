6240ea8125a79d6a87e9c4fe4dcffcc8b0ebae9a
Danya <96995375+tomatorepublic@users.noreply.github.com>
2022-06-10 23:30:50 +0200

    minor visual change in the meaning file

--- meaning.js
+++ meaning.js
@@ -4,7 +4,7 @@ const promises = require('./promises.js');
 
 const main = async () => {
   let word = await promises.question(`\nPlease enter the word - `);
-  console.log(await promises.wordSearch(word.toLowerCase()));
+  console.log(`\n- ${await promises.wordSearch(word.toLowerCase())}`);
   return await promises.wordSearch(word.toLowerCase());
 };
 
